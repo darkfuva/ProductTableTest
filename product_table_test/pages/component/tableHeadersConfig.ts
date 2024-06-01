6feefff7173221a34267cabfaaf0974544a4346a
darkfuva <104079433+darkfuva@users.noreply.github.com>
2024-06-01 21:10:06 +0530

    Finished Integration

---
+++ product_table_test/pages/component/tableHeadersConfig.ts
@@ -0,0 +1,6 @@
+export default [
+    { label: "Title", accessor: "title" },
+    { label: "Brand", accessor: "brand" },
+    { label: "Category", headerClass:"text-right",bodyClass:"text-right", accessor: "category" },
+    { label: "Price", accessor: "price",headerClass:"text-right",bodyClass:"text-right", CellRenderer :(price: number|String)=>`$${price}` },
+];
--- product_table_test/pages/index.tsx
+++ product_table_test/pages/index.tsx
@@ -1,26 +1,85 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import { useEffect } from "react";
+"use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import TableComponent from "./component/tableComponent";
+import tableHeaders from "./component/tableHeadersConfig";
 
-const inter = Inter({ subsets: ["latin"] });
+export type Item = {
+  id: number;
+  title: string;
+  brand: string;
+  category: number;
+  price: number;
+};
 
 export default function Home() {
-  let userObj = {
-    username: "emilys",
-    password  : "emilyspass", 
-    expiresInMins: 30
-  };
+  const router = useRouter();
+  const [items, setItems] = useState<Item[]>([]);
+  const [total, setTotal] = useState<number>(0);
+  const defaultSortBy = "price";
+  const defaultSortDirection = "asc";
+  const defaultLimit = 10;
+  const defaultSkip = 0;
+
   useEffect(() => {
-    fetch("https://dummyjson.com/auth/login", {
+    handleFetchProducts("price", "asc");
+  }, []);
+
+  const handleFetchProducts = (
+    sortBy: string,
+    sortDirection: "asc" | "desc",
+    limit: number = 10,
+    skip: number = 0
+  ) => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      router.push("/login");
+      return;
+    }
+
+    refreshToken(token);
+
+    fetch(
+      `https://dummyjson.com/products?sortBy=${sortBy}&order=${sortDirection}&limit=${limit}&skip=${skip}`
+    )
+      .then((res) => res.json())
+      .then((_items) => {
+        setItems(_items.products);
+        setTotal(_items.total);
+      });
+  };
+
+  const refreshToken = (token: string) => {
+    fetch("https://dummyjson.com/auth/refresh", {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(userObj),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({}),
     })
       .then((res) => res.json())
-      .then(console.log);
-  }, []);
-  return <div>
-    <h1>Login</h1>
-    
-    </div>;
+      .then((user) => localStorage.setItem("token", user.token))
+      .catch((err) => router.push("/login"));
+  };
+
+  return (
+    <div>
+      <div className="flex justify-center p-20 pt-14">
+        <TableComponent
+          totalPages={total}
+          tableHeaders={tableHeaders}
+          onTableStateChange={(sortBy, sortDirection, limit, skip) => {
+            console.log("Called");
+            handleFetchProducts(sortBy, sortDirection, limit, skip);
+          }}
+          tableData={items}
+          defaultSortBy={defaultSortBy}
+          defaultSortDirection={defaultSortDirection}
+          defaultLimit={defaultLimit}
+          defaultSkip={defaultSkip}
+        />
+      </div>
+    </div>
+  );
 }
